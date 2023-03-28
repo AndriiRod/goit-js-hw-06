3701308c954bc56c359252f70973c88a799a7701
AndriiRod <rod.andriiw@gmail.com>
2023-03-28 19:37:58 +0300

    ready first task

---
+++ js/task-01.js
@@ -0,0 +1,8 @@
+const listCategoriesEl = document.querySelectorAll(".item");
+
+console.log(`Number of categories: ${listCategoriesEl.length}`);
+
+listCategoriesEl.forEach((item) => {
+  console.log(`Category: ${item.firstElementChild.textContent}`);
+  console.log(`Elements: ${item.lastElementChild.children.length}`);
+});
