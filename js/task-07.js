658e72f7062619831168a9a9b450179acfb700aa
AndriiRod <rod.andriiw@gmail.com>
2023-03-30 00:49:43 +0300

    ready task7

---
+++ js/task-07.js
@@ -0,0 +1,8 @@
+const fontSizeControl = document.querySelector("#font-size-control");
+const text = document.querySelector("#text");
+
+function resizeText(event) {
+  text.style.fontSize = `${event.currentTarget.value}px`;
+}
+
+fontSizeControl.addEventListener("input", resizeText);
