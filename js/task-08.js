155320e306a9165a628d19f16796d318ea2e13d4
AndriiRod <rod.andriiw@gmail.com>
2023-03-30 12:26:44 +0300

    ready task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,3 +1,18 @@
 const form = document.querySelector(".login-form");
 
+function formSubmit(event) {
+  event.preventDefault();
+  const fildForm = event.currentTarget.elements;
+  if (fildForm.email.value === "" || fildForm.password.value === "")
+    alert("Всі поля повинні бути заповнені!!");
+  else {
+    const user = {
+      email: fildForm.email.value,
+      password: fildForm.password.value,
+    };
+    console.log(user);
+    event.currentTarget.reset();
+  }
+}
+
 form.addEventListener("submit", formSubmit);
