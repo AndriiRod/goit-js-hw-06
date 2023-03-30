cbc8994f8fe6b07e2286e2a002fedcc34a638179
AndriiRod <rod.andriiw@gmail.com>
2023-03-30 12:42:13 +0300

    ready task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,16 @@
+const btn = document.querySelector(".change-color");
+const span = document.querySelector(".color");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function changeBodyColor() {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  span.textContent = randomColor;
+}
+
+btn.addEventListener("click", changeBodyColor);
