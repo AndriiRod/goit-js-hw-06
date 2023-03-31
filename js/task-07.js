a6c014992fb31ea2f128f9e4bae931f384f737f7
AndriiRod <rod.andriiw@gmail.com>
2023-03-31 14:23:23 +0300

    fix trask7

--- js/task-07.js
+++ js/task-07.js
@@ -4,5 +4,5 @@ const text = document.querySelector("#text");
 function resizeText(event) {
   text.style.fontSize = `${event.currentTarget.value}px`;
 }
-
+text.style.fontSize = `${fontSizeControl.value}px`;
 fontSizeControl.addEventListener("input", resizeText);
